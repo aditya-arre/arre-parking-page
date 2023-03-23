0838ade038aad5da2db6a282dc269e088bc82910
Aditya Kumar <theguywithideas@Adityas-MacBook-Pro.local>
2023-03-23 18:40:16 +0530

    first

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,18 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="We are taking a breather and pivoting!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        We are in process of revamping the platform and coming back in a new avatar. Stay tuned! We will back soon!
+
         </p>
+        <p>Thank you for supporting arre.co.in</p>
+        <p>We are active on our social handles. Please follow us on</p>
+        <p><a href="https://www.instagram.com/arreindia/" target="_blank">Instagram</a></p>
+        <p><a href="https://www.youtube.com/c/ArreIndia" target="_blank">YouTube</a></p>
+        <p><a href="https://twitter.com/Arretweets" target="_blank" >Twitter</a></p>
+        <p><a href="https://www.facebook.com/ArreIndia" target="_blank">Facebook</a></p>
+
       </main>
 
       <Footer />
